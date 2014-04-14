9a3881977316333a095a2f41e3a6d53c88febb03
paniczklos <panicz.klos@gmail.com>
2014-04-14 21:05:56 +0200

    Update networks.py

--- p2pool/networks.py
+++ p2pool/networks.py
@@ -8,26 +8,26 @@ from p2pool.util import math
 # changes can be done by changing one, then the other
 
 nets = dict(
-    blackcoin=math.Object(
-        PARENT=networks.nets['blackcoin'],
+    whitecoin=math.Object(
+        PARENT=networks.nets['whitecoin'],
         SHARE_PERIOD=15, # seconds
         CHAIN_LENGTH=12*60*60//15, # shares
         REAL_CHAIN_LENGTH=12*60*60//15, # shares
         TARGET_LOOKBEHIND=20, # shares
         SPREAD=10, # blocks
-        IDENTIFIER='e037d5b8c6923610'.decode('hex'),
-        PREFIX='7208c1a53ef659b0'.decode('hex'),
-        P2P_PORT=8777,
+        IDENTIFIER='10e037d5b8c69236'.decode('hex'),
+        PREFIX='b07208c1a53ef659'.decode('hex'),
+        P2P_PORT=8776,
         MIN_TARGET=0,
         MAX_TARGET=2**256//2**20 - 1,
         PERSIST=True,
-        WORKER_PORT=8336,
+        WORKER_PORT=8343,
         BOOTSTRAP_ADDRS='198.52.200.75'.split(' '),
         ANNOUNCE_CHANNEL='#p2pool-alt',
         VERSION_CHECK=lambda v: v >= 60011,
     ),
-    blackcoin_testnet=math.Object(
-        PARENT=networks.nets['blackcoin_testnet'],
+    whitecoin_testnet=math.Object(
+        PARENT=networks.nets['whitecoin_testnet'],
         SHARE_PERIOD=15, # seconds
         CHAIN_LENGTH=12*60*60//15, # shares
         REAL_CHAIN_LENGTH=12*60*60//15, # shares
